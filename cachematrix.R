9477560935445e5e9b7b4366cd3a2eb070fd3bc9
victoria <srta.vicky@gmail.com>
2014-06-22 00:24:54 -0300

    assignment complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+##function must be called as cacheSolve(makeCacheMatrix(A))
+#beenig A a matrix 
 
-## Write a short comment describing this function
+## create a cache matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
+    #set the value of the vector
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    #get the value of the vector
     get <- function() x
+    #set the value of inverse
     setinv <- function(solve) m <<- solve
+    # get the value of the inverse
     getinv <- function() m
     list(set = set, get = get,
          setinv = setinv,
@@ -19,7 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of a special vector created in the
+#function makeCacheMatrix 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
